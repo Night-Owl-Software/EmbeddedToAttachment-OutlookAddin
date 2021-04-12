11df35f4c372691ad7d8bdc545407099a9cb7cae
Max Courneya <max@mlcgaming.com>
2021-04-12 13:06:37 -0500

    Added comments to application flow for clarification, added line to create a subjet tag when the add-in made changes, and changed default application working folders to local appdata.

--- ThisAddIn.cs
+++ ThisAddIn.cs
@@ -12,38 +12,52 @@ namespace EmbeddedToAttachment
 {
     public partial class ThisAddIn
     {
+        string appFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"Night Owl Software\Outlook Add-ins\Embedded Image to Attachment Add-in");
+        string imageCacheFolder;
+        string settingsFile;
+        string blacklistFile;
+
         Outlook.NameSpace outlookNameSpace;
         Outlook.MAPIFolder inbox;
         Outlook.Items items;
 
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
-            
+            // Create our base directory and file paths
+            imageCacheFolder = Path.Combine(appFolder, @"image-cache");     // Directory for storing temporary images while converting them to attachments
+            settingsFile = Path.Combine(appFolder, "settings.xml");         // A file used to store user-specified settings, for future implementation
+            blacklistFile = Path.Combine(appFolder, "blacklist.xml");       // A file used to store email addresses that this add-in will ignore, for future implementation
 
+            // Create the AppData and ImageCache folders, if needed
+            Directory.CreateDirectory(appFolder);
+            Directory.CreateDirectory(imageCacheFolder);
+
+            // Get a List of all files still in the Image Cache
+            string[] allFiles = Directory.GetFiles(imageCacheFolder);
+
+            // Clear the Image Cache, if any images exist
+            if(allFiles.Length > 0) {
+                for(int i = allFiles.Length - 1; i >= 0; i--) {
+                    File.Delete(allFiles[i]);
+                }
+            }
+
+            // Get the Outlook Namespace so we can monitor incoming messages
             outlookNameSpace = this.Application.GetNamespace("MAPI");
             inbox = outlookNameSpace.GetDefaultFolder(
                     Outlook.OlDefaultFolders.olFolderInbox);
 
+            // Get a list of inbox items and add an event flag for when new items are added (a message is received)
             items = inbox.Items;
             items.ItemAdd +=
                 new Outlook.ItemsEvents_ItemAddEventHandler(items_ItemAdd);
         }
 
-        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
-        {
-            // Note: Outlook no longer raises this event. If you have code that 
-            //    must run when Outlook shuts down, see https://go.microsoft.com/fwlink/?LinkId=506785
-        }
-
         void items_ItemAdd(object Item) {
             Outlook.MailItem mail = (Outlook.MailItem)Item;
-            if(Item != null) {
-                
-                if(mail.Attachments.Count > 0) {
 
-                    if(Directory.Exists(@"C:\Temp") == false) {
-                        Directory.CreateDirectory(@"C:\Temp");
-                    }
+            if(Item != null) {                
+                if(mail.Attachments.Count > 0) {
 
                     List<Outlook.Attachment> imageAttachments = new List<Outlook.Attachment>();
 
@@ -59,15 +73,21 @@ namespace EmbeddedToAttachment
                         }
                     }
 
-                    foreach(Outlook.Attachment image in imageAttachments) {
-                        string fileName = image.FileName;
-                        image.SaveAsFile($"C:\\Temp\\{fileName}");
-                        AddAttachment(mail, fileName);
-                    }
+                    if(imageAttachments.Count > 0) {
+                        string tempSubject = mail.Subject;
 
-                    foreach(Outlook.Attachment image in imageAttachments) {
-                        string fileName = image.FileName;
-                        File.Delete($"C:\\Temp\\{fileName}");
+                        mail.Subject = $"[ATTACHMENTS] {tempSubject}";
+
+                        foreach(Outlook.Attachment image in imageAttachments) {
+                            string fileName = image.FileName;
+                            image.SaveAsFile($"{imageCacheFolder}\\{fileName}");
+                            AddAttachment(mail, fileName);
+                        }
+
+                        foreach(Outlook.Attachment image in imageAttachments) {
+                            string fileName = image.FileName;
+                            File.Delete($"{imageCacheFolder}\\{fileName}");
+                        }
                     }
                 }
             }
@@ -76,7 +96,7 @@ namespace EmbeddedToAttachment
 
         private void AddAttachment(Outlook.MailItem mail, string fileName) {
             if(fileName.Length > 0) {
-                mail.Attachments.Add($"C:\\Temp\\{fileName}", Outlook.OlAttachmentType.olByValue, mail.Attachments.Count + 1, fileName);
+                mail.Attachments.Add($"{imageCacheFolder}\\{fileName}", Outlook.OlAttachmentType.olByValue, mail.Attachments.Count + 1, fileName);
             }
         }
 
@@ -89,7 +109,6 @@ namespace EmbeddedToAttachment
         private void InternalStartup()
         {
             this.Startup += new System.EventHandler(ThisAddIn_Startup);
-            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
         }
         
         #endregion
